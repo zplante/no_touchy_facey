6570474a2e6ba06fe4b4b40351c0d18b2d61fbe1
Zac P <35752336+zplante@users.noreply.github.com>
2020-07-28 14:18:55 -0700

    Add files via upload

---
+++ hand_detector.py
@@ -0,0 +1,50 @@
+
+import cv2
+from utils import detector_utils as detector_utils
+
+
+############################################################################
+class Detector:
+    detector_params = {}
+    detector = None
+
+    def __init__(self):
+        pass
+
+    def set_detector_params(self, params):
+        self.detector_params = params
+
+    def detect(self):
+        pass
+
+
+############################################################################
+class CVHaarcascadeDetector(Detector):
+    def __init__(self, xml_filename):
+        self.detector = cv2.CascadeClassifier(xml_filename)
+
+    def detect(self, rgb_img):
+        # https://stackoverflow.com/questions/20801015/recommended-values-for-opencv-detectmultiscale-parameters
+        # returns an array of bounding boxes (x, y, w, h)
+        scale = self.detector_params.get('scale', 1.05)
+        min_neighbors = self.detector_params.get('min_neighbors', 6)
+        gray_img = cv2.cvtColor(rgb_img, cv2.COLOR_RGB2GRAY)
+        return self.detector.detectMultiScale(gray_img, scale, min_neighbors)
+
+
+##################################################################################
+class TSDetector(Detector):
+    def __init__(self):
+        self.detection_graph, self.sess = detector_utils.load_inference_graph()
+
+    def detect(self, rgb_image):
+        # returns (top [0], left [1], bottom [2], right [3])
+        boxes, confidences = detector_utils.detect_objects(rgb_image, self.detection_graph, self.sess)
+
+        im_height, im_width = rgb_image.shape[:2]
+
+        detection_th = self.detector_params.get('detection_th', 0.5)
+        objects = [(box[0] * im_height, box[3] * im_width, box[2] * im_height, box[1] * im_width) for box, score in zip(boxes, confidences) if score >= detection_th]
+        # change to an array of (x, y, w, h)
+        return [(int(left), int(top), int(right - left), int(bottom - top)) for (top, right, bottom, left) in objects]
+
